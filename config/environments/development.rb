56bd5aa8904758e1a0d18c9df2415faf0a931e8f
Evgeny Lapin <lev@openteam.ru>
2015-09-01 10:36:05 +0600

    security update rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Students::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Temporarily enable caching in development (COMMENT OUT WHEN DONE!)
+  # config.action_controller.perform_caching = true
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
