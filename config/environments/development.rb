070f70199961901daaf61d56d41efd847f4b4e59
Evgeny Lapin <lev@openteam.ru>
2015-09-02 08:49:55 +0600

    updated savon & nokogiri gems; disabled rails cache in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,9 @@ Students::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Temporarily enable caching in development (COMMENT OUT WHEN DONE!)
-  # config.action_controller.perform_caching = true
+  # Disable caching in development
+  config.perform_caching = false
+  config.cache_store = :null_store
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
