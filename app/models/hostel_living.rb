0ceb610d7dcb560b3c06ff5ce423befb91948818
Evgeniy Lapin <lev@openteam.ru>
2018-03-28 18:30:34 +0700

    rescue from wrong hash

--- app/models/hostel_living.rb
+++ app/models/hostel_living.rb
@@ -7,19 +7,23 @@ class HostelLiving < Model
 
   class << self
     def from(hash)
-      vals = %i[student_hoste_id hostel room_number start_date end_date]
-      student_hostel_id, hostel, room_number, start_date, end_date = hash.values_at(*vals)
-      HostelLiving.new(
-        student_hostel_id: student_hostel_id,
-        hostel: hostel,
-        room_number: room_number,
-        start_date: start_date,
-        end_date: end_date
-      )
+      begin
+        vals = %i[student_hoste_id hostel room_number start_date end_date]
+        student_hostel_id, hostel, room_number, start_date, end_date = hash.values_at(*vals)
+        HostelLiving.new(
+          student_hostel_id: student_hostel_id,
+          hostel: hostel,
+          room_number: room_number,
+          start_date: start_date,
+          end_date: end_date
+        )
+      rescue
+        nil
+      end
     end
 
     def from_array(array_of_hashes)
-      array_of_hashes.map{|hash| from(hash) }
+      array_of_hashes.map{ |hash| from(hash) }.compact
     end
   end
 end
