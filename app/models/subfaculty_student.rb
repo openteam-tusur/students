a4b3788d1fb8f8f749cc0ad32d6ba9176374e771
Karmickoala904 <karmickoala904@gmail.com>
2018-04-12 13:28:13 +0700

    add group info to subfaculty students

--- app/models/subfaculty_student.rb
+++ app/models/subfaculty_student.rb
@@ -1,15 +1,17 @@
 class SubfacultyStudent < Model
   attribute :student_id
   attribute :status
+  attribute :group
 
   class << self
     def from(hash)
       begin
-        vals = %i[study_id status_name]
-        student_id, status = hash.values_at(*vals)
+        vals = %i[study_id status_name group_name]
+        student_id, status, group = hash.values_at(*vals)
         SubfacultyStudent.new(
           student_id: student_id,
-          status: status
+          status: status,
+          group: group
         )
       rescue
         nil
