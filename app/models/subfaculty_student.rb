efd42dd9845031c536ada4687ec8ad2b62bb0cc7
Karmickoala904 <karmickoala904@gmail.com>
2018-04-17 11:07:48 +0700

    savruk want more information

--- app/models/subfaculty_student.rb
+++ app/models/subfaculty_student.rb
@@ -2,16 +2,23 @@ class SubfacultyStudent < Model
   attribute :student_id
   attribute :status
   attribute :group
+  attribute :fullname
+  attribute :course
+  attribute :education_info
 
   class << self
     def from(hash)
       begin
-        vals = %i[study_id status_name group_name]
-        student_id, status, group = hash.values_at(*vals)
+        vals = %i[study_id status_name group_name course edu_level]
+        student_id, status, group, course, education_info = hash.values_at(*vals)
+        fullname = %(#{hash[:surname]} #{hash[:name]} #{hash[:patronymic]})
         SubfacultyStudent.new(
           student_id: student_id,
           status: status,
-          group: group
+          group: group,
+          fullname: fullname,
+          course: course,
+          education_info: education_info
         )
       rescue
         nil
