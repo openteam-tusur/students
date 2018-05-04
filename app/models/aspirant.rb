0ec5eb3826910f0949b11c72026b4131335da781
Evgeniy Lapin <lev@openteam.ru>
2018-05-04 13:11:37 +0700

    extend aspirant json for equai key with student json

--- app/models/aspirant.rb
+++ app/models/aspirant.rb
@@ -43,7 +43,69 @@ class Aspirant
       firstname: item['FirstName'],
       lastname: item['LastName'],
       patronymic: item['MiddleName'],
-      learns: true
+      gender: item['Sex'].gsub('М', 'мужской').gsub('Ж', 'женский'),
+      financing: item['FinanceType'].to_s.gsub('1', 'Бюджет').gsub('2', 'ПВЗ'),
+      learns: true,
+      education: {
+        params: {
+          edu_id: nil,
+          edu_form: {
+            edu_form_id: item['EduForm'].try(:[], 'EduFormId'),
+            edu_form_name: item['EduForm'].try(:[], 'EduFormName')
+          },
+          sub_faculty: {
+            sub_faculty_id: item['ChairCard'].try(:[], 'SubFacultyId'),
+            sub_faculty_name: item['ChairCard'].try(:[], 'SubFacultyName'),
+            short_name: item['ChairCard'].try(:[], 'ShortName')
+          },
+          faculty: {
+            faculty_id: item['Group'].try(:[], 'Education').try(:[], 'Faculty').try(:[], 'FacultyId'),
+            faculty_name: item['Group'].try(:[], 'Education').try(:[], 'Faculty').try(:[], 'FacultyName'),
+            short_name: item['Group'].try(:[], 'Education').try(:[], 'Faculty').try(:[], 'ShortName')
+          },
+          speciality: {
+            speciality_id: item['SpecialityCard'].try(:[], 'SpecialityId'),
+            speciality_code: item['SpecialityCard'].try(:[], 'SpecialityCode'),
+            speciality_name: item['SpecialityCard'].try(:[], 'SpecialityDirName'),
+            speciality_dir_code: item['DirectionCode'],
+            speciality_dir_name: item['SpecialityCard'].try(:[], 'SpecialityName'),
+            speciality_type_name: 'аспирантура'
+          },
+          is_active: true,
+          group_id: item['Group'].try(:[], 'GroupId'),
+          group_name: item['Group'].try(:[], 'GroupName'),
+          year_forming: item['Group'].try(:[], 'YearForming'),
+          course: item['Group'].try(:[], 'Course'),
+          semestre: item['Group'].try(:[], 'Semestre'),
+          years_count: item['Group'].try(:[], 'EduYearsCount')
+        }
+      },
+      group: {
+        number: item['Group'].try(:[], 'GroupName'),
+        course: item['Group'].try(:[], 'Course'),
+        year_forming: item['Group'].try(:[], 'YearForming'),
+        education_form: {
+          kind: item['Group'].
+            try(:[], 'Education').
+            try(:[], 'EduForm').
+            try(:[], 'EduFormName').
+            gsub('Очная', 'full-time').
+            gsub('Заочная', 'postal')
+        },
+        speciality: {
+          code: item['SpecialityCard'].try(:[], 'SpecialityCode'),
+          name: item['SpecialityCard'].try(:[], 'SpecialityDirName'),
+          kind: 'аспирантура',
+        },
+        subfaculty: {
+          name: item['ChairCard'].try(:[], 'SubFacultyName'),
+          abbr: item['ChairCard'].try(:[], 'ShortName'),
+          faculty: {
+            name: item['Group'].try(:[], 'Education').try(:[], 'Faculty').try(:[], 'FacultyName'),
+            abbr: item['Group'].try(:[], 'Education').try(:[], 'Faculty').try(:[], 'ShortName')
+          }
+        }
+      }
     }
   end
 end
